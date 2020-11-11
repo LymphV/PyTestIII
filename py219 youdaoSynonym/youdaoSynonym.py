d493e691fb81592f8aef22679c75f57c13f4d60b
LymphV <470481777@qq.com>
2020-11-11 18:06:40 +0800

    有道更新
    繁体字视为日语报错问题，
    中括号被劈开问题，
    全角小括号问题，
    (iknow，点击我知道了，上次

--- py219 youdaoSynonym/youdaoSynonym.py
+++ py219 youdaoSynonym/youdaoSynonym.py
@@ -10,6 +10,9 @@ youdaoSynonym
 {'Optical computing', 'optical calculation', 'optical computing'}
 >>>yd.close() ###关闭浏览器
 '''
+import re
+from time import sleep
+from selenium.common.exceptions import TimeoutException
 
 if '.' in __name__:
     from .driverOps import getDriver, waitTillOpen 
@@ -17,6 +20,98 @@ if '.' in __name__:
 else:
     from driverOps import getDriver, waitTillOpen
     from cfg import home
+
+
+
+class YoudaoFilter:
+    '''
+    同义词过滤器
+    '''
+    ###词性表
+    __cixings = ['n', 'pron', 'prep', 'adj', 'adv', 'v', 'num',
+              'art', 'conj', 'int', 'abbr']
+
+    ###停用词表
+    __stopWords = {'the', 'a', 'an',}
+    
+    def filterLower (s : str) -> set:
+        '''
+        大写转小写
+        '''
+        return {s.lower()}
+
+    def filterTerm (s : str) -> set:
+        '''
+        去除中括号的术语标志
+        '''
+        s = re.sub(r'\[.+?\]', ' ', s)
+        s = re.sub(r'\[.*?$', '', s)
+        s = re.sub(r'^.*?]', '', s)
+        return {s}
+
+    def filterBracket (s : str) -> set:
+        '''
+        去除小括号
+        '''
+        s = re.sub(r'\(.+?\)', ' ', s)
+        s = re.sub(r'（.+?）', ' ', s)
+        return {s}
+
+    def filterCixing (s : str) -> set:
+        '''
+        去除词性标志
+        '''
+        s = re.sub('|'.join([f'({x}\.)' for x in YoudaoFilter.__cixings]), ' ', s)
+        s = re.sub('\.\s*$', '', s)
+        return {s}
+
+    def filterComma (s : str) -> set:
+        '''
+        去除逗号
+        '''
+        return {s.replace(',', ' ')}
+
+    def filterSemicolon (s : str) -> set:
+        '''
+        拆分分号
+        '''
+        return set(s.replace(';','\n').replace('；','\n').split('\n'))
+
+    def filterHyphen (s : str) -> set:
+        '''
+        生成无连字符词
+        '''
+        return {s, s.replace('-', ' ')}
+
+    def filterStopWords (s : str) -> set:
+        '''
+        去除停用词
+        '''
+        return {' '.join([x for x in s.split() if x and x not in YoudaoFilter.__stopWords])}
+
+    def filterStrip (s : str) -> set:
+        '''
+        去除空串
+        '''
+        s = s.strip()
+        return {s} if s else set()
+    
+    filters = [filterLower, filterTerm, filterBracket, filterCixing, filterComma, ###去除一些东西（整体上）
+                 filterHyphen, filterSemicolon,###扩展一些东西
+                 filterStopWords,###去除一些词（分词后）
+                 filterStrip] ###去重去空
+    
+    def filter (ss : set, filters) -> set:
+        '''
+        过滤器
+        按过滤器顺序处理字符串集合
+        '''
+        for f in filters:
+            rst = set()
+            for s in ss:
+                rst |= f(s)
+            ss = rst
+        return ss
 
 
 class YoudaoSynonym:
@@ -32,10 +127,12 @@ class YoudaoSynonym:
     >>>yd.close() ###关闭浏览器，必须
     '''
     
-    __version__ = 20201029
+    __version__ = 20201111
     __author__ = 'LymphV@163.com'
+    
+    __filters = YoudaoFilter.filters
 
-    def __init__ (this, page = home):
+    def __init__ (this, page = home, ifHeadless = True):
         '''
         YoudaoSynonym初始化
         
@@ -44,9 +141,26 @@ class YoudaoSynonym:
         page : 有道翻译网址，默认为cfg.py中的home
         '''
         this.__dv = None
+        this.__ifHeadless = ifHeadless
         this.start(page)
     
     
+    def __iknow (this):
+        pathIKnow = '//a[@class="i-know"]/../../div[contains(@style,"block")]//a[@class="i-know"]'
+        iknow = this.__dv.find_elements_by_xpath(pathIKnow)
+        if iknow: iknow[0].click()
+    
+    def __ch2en (this):
+        pathLang = '//*[@class="select-text"]'
+        pathCh2En = '//*[@data-value="zh-CHS2en"]/../../*[contains(@style,"block")]/*[@data-value="zh-CHS2en"]'
+        
+        dv = this.__dv
+        waitTillOpen(dv, 10, value=pathLang)
+        dv.find_element_by_xpath(pathLang).click()
+        waitTillOpen(dv, 10, value=pathCh2En)
+        dv.find_element_by_xpath(pathCh2En).click()
+        
+        
     def start (this, page = home):
         '''
         打开有道翻译网址
@@ -57,28 +171,42 @@ class YoudaoSynonym:
         '''
         if not page: return
         if not this.__dv:
-            this.__dv = getDriver()
+            this.__dv = getDriver(this.__ifHeadless)
         this.__dv.get(page)
+        this.__iknow()
+        #this.__ch2en()
+        
     
     def __getSynonyms (this, s : str):
         '''
         使用有道翻译获得同义词集
         '''
         dv = this.__dv
-        dv.refresh()
+        #dv.refresh()
         pathInput = '//textarea[@class="input__original__area"]'
         pathAnswer = '//div[@class="input__target__text"]/p/span'
-        pathSuggest = '//*[@systemname="sys%d"]'
+        pathSuggestWait = '//*[@class="suggest__title"]/../../*[contains(@style,"block")]'
+        pathSuggest = '//*[@class="suggest__title"]/../ul/*'
         pathRelative = '//div[@class="dict__relative"]/*'
+        pathTrans = '//a[@id="transMachine"]'
         
         
         ###输入
         waitTillOpen(dv, 10, value=pathInput)
         ipts = dv.find_elements_by_xpath(pathInput)
-        assert ipts, 'no input area found'
+        
+        this.__iknow()
         
         ipt = ipts[0]
         ipt.clear()
+        
+        for i in range(10):
+            ans = dv.find_elements_by_xpath(pathAnswer)
+            if not ans: break
+            dv.find_element_by_xpath(pathTrans).click()
+            sleep(0.1)
+        else: assert 0, 'translate area not cleared'
+        
         ipt.send_keys(s)
         
         rst = set()
@@ -90,25 +218,27 @@ class YoudaoSynonym:
         if ans:
             ###翻译改进结果
             ans[0].click()
-            for i in range(1,1000):
-                sug = dv.find_elements_by_xpath(pathSuggest % i)
-                if not sug: break
+            
+            try: 
+                waitTillOpen(dv, 10, value=pathSuggestWait)
+                sug = dv.find_elements_by_xpath(pathSuggest)
                 rst |= {x.text for x in sug}
+            except TimeoutException: pass
         
         ###翻译相关结果
         relative = dv.find_elements_by_xpath(pathRelative)
         rst |= {x.text for x in relative}
+        ipt.clear()
+        dv.find_element_by_xpath(pathTrans).click()
         return rst
     
-    def __filter (this, s : str) -> set:
+    def __filter (this, ss : set) -> set:
         '''
         过滤器
-        过滤掉短语中的标点符号、停用词、词性、专业标志等
-        ····待开发
+        过滤掉短语中的标点符号、停用词、词性、术语标志等
         '''
-        s = s.strip()
-        return {s} if s else set()
-        
+        return YoudaoFilter.filter(ss, YoudaoSynonym.__filters)
+
     
     def getSynonyms (this, s : str) -> set:
         '''
@@ -122,14 +252,11 @@ class YoudaoSynonym:
         -------
         getSynonyms : 英文同义词集合
         '''
-        syns = this.__getSynonyms(s)
-        rst = set()
-        for s in syns:
-            rst |= this.__filter(s)
-        return rst
+        return this.__filter(this.__getSynonyms(s))
     def close (this):
         '''
         关闭浏览器，必须
         '''
-        this.__dv.quit()
-        this.__dv = None
+        if this.__dv:
+            this.__dv.quit()
+            this.__dv = None
