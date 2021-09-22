dce6f3f5d8cc23dcfb76f54ec85a53d52ea91c34
LymphV <37726541+LymphV@users.noreply.github.com>
2021-09-22 18:29:11 +0800

    Update cfg.py

--- py232 esScholar/cfg.py
+++ py232 esScholar/cfg.py
@@ -11,14 +11,3 @@ ncols = 950
 port = 9200
 master = f'http://{host}:{port}'
 contType = '-H "content-Type:application/json"'
-
-
-### 阿里云
-dbHost = 'rds-inner.mysql.zhangbei.rds.aliyuncs.com'
-dbUser = 'landinn_es'
-dbPwd = 'AsdfQwpo_369'
-
-### 47
-# dbHost = '10.208.63.47'
-# dbUser = 'root'
-# dbPwd = 'linlei'
