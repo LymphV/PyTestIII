a76561f243bee85b39f166764ab95ae707d71751
LymphV <470481777@qq.com>
2022-07-13 15:47:43 +0800

    同义词

---
+++ py231 field&term syn/simpleAndTranditional.py
@@ -0,0 +1,22 @@
+if '.' in __name__:
+    from .langconv import Converter
+else:
+    from langconv import Converter
+
+def trd2smp(sentence):
+    '''
+    将sentence中的繁体字转为简体字
+    :param sentence: 待转换的句子
+    :return: 将句子中繁体字转换为简体字之后的句子
+    '''
+    sentence = Converter('zh-hans').convert(sentence)
+    return sentence
+
+def smp2trd(sentence):
+    '''
+    将sentence中的简体字转为繁体字
+    :param sentence: 待转换的句子
+    :return: 将句子中简体字转换为繁体字之后的句子
+    '''
+    sentence = Converter('zh-hant').convert(sentence)
+    return sentence
