6e7416ac753edd92f210c852a51db1d8a8d76284
LymphV <470481777@qq.com>
2022-07-13 15:47:21 +0800

    ucas

---
+++ py235 ucasClimbed/cfg.py
@@ -0,0 +1,14 @@
+'''
+
+配置文件
+包括
+    userAgent : 翻译所使用的浏览器的userAgent
+    home : 主页
+    
+'''
+
+###要使用所使用的浏览器的userAgent
+userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+
+###主页
+home = 'https://www.ucas.ac.cn/site/74'
